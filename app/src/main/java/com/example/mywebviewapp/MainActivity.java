3106df0ad49980ceb0922d1e25818dfd60449269
PowerTech0417 <162703471+PowerTech0417@users.noreply.github.com>
2025-11-18 07:26:39 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/example/mywebviewapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mywebviewapp;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://powertech0417.github.io/Cv_ap/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
