5fe25169b54c0228707da2d6e7773f80f32282a6
PowerTech0417 <162703471+PowerTech0417@users.noreply.github.com>
2025-11-18 09:15:48 +0800

    Update MainActivity.java

--- app/src/main/java/com/powertech0417/cvapp/MainActivity.java
+++ app/src/main/java/com/powertech0417/cvapp/MainActivity.java
@@ -1,33 +1,24 @@
-package com.example.mywebviewapp;
-import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
+package com.powertech0417.cvapp;
 
-public class MainActivity extends AppCompatActivity {
-    private WebView myWebView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        myWebView = findViewById(R.id.webview);
-        WebSettings webSettings = myWebView.getSettings();
+        
+        webView = new WebView(this);
+        setContentView(webView);
+        
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
-
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("https://powertech0417.github.io/Cv_ap/");
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (myWebView.canGoBack()) {
-            myWebView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        
+        webView.loadUrl("https://powertech0417.github.io/Cv_ap/");
     }
 }
