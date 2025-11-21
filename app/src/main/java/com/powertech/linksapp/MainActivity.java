2468196545d16448aa78f3f387bc843d4e4a120d
PowerTech0417 <162703471+PowerTech0417@users.noreply.github.com>
2025-11-21 23:41:08 +0800

    Update MainActivity.java

--- app/src/main/java/com/powertech/linksapp/MainActivity.java
+++ app/src/main/java/com/powertech/linksapp/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     private ProgressBar progressBar;
-    private static final String TARGET_URL = "https://link-1ku.pages.dev/";
+    private static final String TARGET_URL = "https://ottlinkpw.pages.dev/";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
